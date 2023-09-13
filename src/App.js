633ce4209f47b9ef6c919d2e0c4e1c178ea98822
amishra469 <svmaditya01@gmail.com>
2023-09-13 16:01:07 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import './App.css';
+import React from 'react'
+import Header from './components/Header/Header'
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      Plot Routes on Map
+    </div>
+  )
+}
+
+export default Header
