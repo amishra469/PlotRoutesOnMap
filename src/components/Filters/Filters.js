a373dadad723e6e5bb96bbf5aef55be16716f0cf
amishra469 <svmaditya01@gmail.com>
2024-11-11 23:13:06 +0530

    Updated Code with new npm version

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,14 +1,48 @@
-import React, { useState } from 'react'
-import sourceIcon from "../../assets/Source.png"
-import destinationIcon from "../../assets/Destination.png"
-import "./Filters.css"
+import React, { useState } from 'react';
+import axios from 'axios';
+import sourceIcon from "../../assets/Source.png";
+import destinationIcon from "../../assets/Destination.png";
+import "./Filters.css";
 
-const Filters = ({source, destination, setSource, setDestination, setSrc, setDest, handleRoute}) => {
-    const [isSource, setIsSource] = useState(false)
-    const [isDestination, setIsDestination] = useState(false)
+const Filters = ({ source, destination, setSource, setDestination, setSrc, setDest, handleRoute }) => {
+    const [isSource, setIsSource] = useState(false);
+    const [isDestination, setIsDestination] = useState(false);
+    const [search, setSearch] = useState("");
+
+    // Function to fetch coordinates from location name
+    const fetchCoordinates = async () => {
+        try {
+            const response = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${search}`);
+            if (response.data.length > 0) {
+                const { lat, lon } = response.data[0];
+                const coordinates = `${lat},${lon}`;
+                setSource(coordinates); // set the source or destination based on your logic
+                setSrc(coordinates);    // update the map with the fetched coordinates
+                alert("Coordinates found: " + coordinates);
+            } else {
+                alert("Location not found. Try another name.");
+            }
+        } catch (error) {
+            console.error("Error fetching coordinates:", error);
+        }
+    };
 
     return (
         <div>
+            <div>
+                <div>Search Location</div>
+                <div className='input-container'>
+                    <input
+                        className='input-box'
+                        type='text'
+                        placeholder='Enter location name...'
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <button onClick={fetchCoordinates}>Search</button>
+                </div>
+            </div>
+
             <div>
                 <div>Source</div>
                 <div className='input-container'>
@@ -18,12 +52,13 @@ const Filters = ({source, destination, setSource, setDestination, setSrc, setDes
                         value={source}
                         onChange={(e) => setSource(e.target.value)}
                     />
-                    <span onClick={() => {setIsSource(true); setSrc(source)}}>
+                    <span onClick={() => { setIsSource(true); setSrc(source); }}>
                         <img className={!isSource ? 'map-marker' : 'map-marker source-marker-clicked'}
                             src={sourceIcon} alt='source'></img>
                     </span>
                 </div>
-            </div >
+            </div>
+
             <div>
                 <div>Destination</div>
                 <div className='input-container'>
@@ -33,15 +68,16 @@ const Filters = ({source, destination, setSource, setDestination, setSrc, setDes
                         value={destination}
                         onChange={(e) => setDestination(e.target.value)}
                     />
-                    <span onClick={() => {setIsDestination(true); setDest(destination)}}>
+                    <span onClick={() => { setIsDestination(true); setDest(destination); }}>
                         <img className={!isDestination ? 'map-marker' : 'map-marker destination-marker-clicked'}
-                            src={destinationIcon} alt='source'></img>
+                            src={destinationIcon} alt='destination'></img>
                     </span>
                 </div>
             </div>
-            <button type='submit' onClick={()=> {handleRoute(source, destination)}}>Get The Route</button>
-        </div >
-    )
-}
 
-export default Filters
+            <button type='submit' onClick={() => { handleRoute(source, destination); }}>Get The Route</button>
+        </div>
+    );
+};
+
+export default Filters;
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -25,8 +25,7 @@ const Map = () => {
             map = L.map('mymap').setView([22.027718, 83.79754], 5);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
-                attribution:
-                    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             }).addTo(map);
             mapRef.current = map;
         }
@@ -37,125 +36,99 @@ const Map = () => {
             iconUrl: markerType === 'source' ? sourceIcon : destinationIcon,
             iconSize: [25, 30],
         });
-        return markerIcon
-    }
+        return markerIcon;
+    };
 
     const plotMarker = (markerType, coords) => {
         if (!map || !coords) return;
 
         const marker = new L.Marker(coords, { icon: getMarker(markerType), draggable: true });
-
         if (markerType === 'source') {
-            if (sourceMarker) map.removeLayer(sourceMarker)
-            sourceMarker = marker;
-            if (routingControl) {
-                const waypoints = routingControl.getWaypoints();
-                waypoints[0].latLng = sourceMarker.getLatLng(); // Update source waypoint
-                routingControl.setWaypoints(waypoints);
+            if (sourceMarker) {
+                sourceMarker.setLatLng(coords);
+            } else {
+                sourceMarker = marker;
+                sourceMarker.addTo(map);
+            }
+        } else if (markerType === 'destination') {
+            if (destinationMarker) {
+                destinationMarker.setLatLng(coords);
+            } else {
+                destinationMarker = marker;
+                destinationMarker.addTo(map);
             }
         }
-        else if (markerType === 'destination') {
-            if (destinationMarker) map.removeLayer(destinationMarker)
-            destinationMarker = marker;
-            if (routingControl) {
-                const waypoints = routingControl.getWaypoints();
-                waypoints[waypoints.length - 1].latLng = destinationMarker.getLatLng(); // Update destination waypoint
-                routingControl.setWaypoints(waypoints);
-            }
-        }
-
-        map.addLayer(marker);
     };
 
     const setSrc = (src) => {
         setSource(src);
-        if (src !== '') {
-            const [lat, lng] = src.split(',');
-            if (isValidLatitude(lat) && isValidLongitude(lng))
-                plotMarker('source', [lat, lng]);
-            else
-                alert("Please insert valid lattitude and longitude")
+        const [lat, lng] = src.split(',');
+        if (isValidLatitude(lat) && isValidLongitude(lng)) {
+            plotMarker('source', [lat, lng]);
+        } else {
+            alert("Please insert valid latitude and longitude");
         }
     };
 
     const setDest = (dest) => {
         setDestination(dest);
-        if (dest !== '') {
-            const [lat, lng] = dest.split(',');
-            if (isValidLatitude(lat) && isValidLongitude(lng))
-                plotMarker('destination', [lat, lng]);
-            else
-                alert("Please insert valid lattitude and longitude")
-
+        const [lat, lng] = dest.split(',');
+        if (isValidLatitude(lat) && isValidLongitude(lng)) {
+            plotMarker('destination', [lat, lng]);
+        } else {
+            alert("Please insert valid latitude and longitude");
         }
     };
 
     const handleRoute = () => {
-        if (source !== '' && destination !== '') {
-            let [sLat, sLng] = source.split(",");
-            let [dLat, dLng] = destination.split(",")
-
-            debugger;
-            if (!isValidLatitude(sLat) || !isValidLongitude(sLng)) {
-                alert("Please insert valid lattitude and longitude");
+        if (source && destination) {
+            const [sLat, sLng] = source.split(",");
+            const [dLat, dLng] = destination.split(",");
+    
+            if (!isValidLatitude(sLat) || !isValidLongitude(sLng) || !isValidLatitude(dLat) || !isValidLongitude(dLng)) {
+                alert("Please insert valid latitude and longitude");
                 return;
             }
-
-            if (!isValidLatitude(dLat) || !isValidLongitude(dLng)) {
-                alert("Please insert valid lattitude and longitude");
-                return;
-            }
-
+    
             if (routingControl) {
-                map.removeControl(routingControl);
-            }
-
-            if (sourceMarker) map.removeLayer(sourceMarker)
-            if (destinationMarker) map.removeLayer(destinationMarker)
-
-            // Create markers with custom icons and make them draggable
-            sourceMarker = L.marker([parseFloat(sLat), parseFloat(sLng)], { icon: getMarker('source'), draggable: true });
-            destinationMarker = L.marker([parseFloat(dLat), parseFloat(dLng)], { icon: getMarker('destination'), draggable: true });
-
-
-            // Create a routing control with the waypoints
-            if (routingControl) {
-                map.removeControl(routingControl);
-            }
-
-            routingControl = L.Routing.control({
-                waypoints: [
+                routingControl.setWaypoints([
                     L.latLng(parseFloat(sLat), parseFloat(sLng)),
                     L.latLng(parseFloat(dLat), parseFloat(dLng))
-                ],
-                routeWhileDragging: true,
-                createMarker: function (i, waypoint, n) {
-                    if (i === 0) {
-                        return sourceMarker;
-                    } else if (i === n - 1) {
-                        return destinationMarker;
+                ]);
+            } else {
+                routingControl = L.Routing.control({
+                    waypoints: [
+                        L.latLng(parseFloat(sLat), parseFloat(sLng)),
+                        L.latLng(parseFloat(dLat), parseFloat(dLng))
+                    ],
+                    routeWhileDragging: true,
+                    createMarker: (i, waypoint, n) => {
+                        if (i === 0) return sourceMarker;
+                        if (i === n - 1) return destinationMarker;
                     }
-                }
-            }).addTo(map);
-
-            // Listen for marker drag events and update waypoints
-            sourceMarker.on('dragend', function () {
-                const waypoints = routingControl.getWaypoints();
-                waypoints[0].latLng = sourceMarker.getLatLng(); // Update source waypoint
-                let lat = sourceMarker.getLatLng().lat, lng = sourceMarker.getLatLng().lng
-                setSource(lat.toFixed(2) + "," + lng.toFixed(3))
-                routingControl.setWaypoints(waypoints);
+                }).addTo(map);
+            }
+    
+            // Updated `dragend` event for source marker
+            sourceMarker.on('dragend', () => {
+                const newSourceLatLng = sourceMarker.getLatLng();
+                const newSourceCoords = `${newSourceLatLng.lat.toFixed(2)},${newSourceLatLng.lng.toFixed(3)}`;
+                setSource(newSourceCoords);  // Update the input field with the new coordinates
+                routingControl.getWaypoints()[0].latLng = newSourceLatLng;
+                routingControl.setWaypoints(routingControl.getWaypoints()); // Refresh route with updated waypoint
             });
-
-            destinationMarker.on('dragend', function () {
-                const waypoints = routingControl.getWaypoints();
-                waypoints[waypoints.length - 1].latLng = destinationMarker.getLatLng(); // Update destination waypoint
-                let lat = destinationMarker.getLatLng().lat, lng = destinationMarker.getLatLng().lng
-                setDestination(lat.toFixed(2) + "," + lng.toFixed(3))
-                routingControl.setWaypoints(waypoints);
+    
+            // Updated `dragend` event for destination marker
+            destinationMarker.on('dragend', () => {
+                const newDestLatLng = destinationMarker.getLatLng();
+                const newDestCoords = `${newDestLatLng.lat.toFixed(2)},${newDestLatLng.lng.toFixed(3)}`;
+                setDestination(newDestCoords);  // Update the input field with the new coordinates
+                routingControl.getWaypoints()[1].latLng = newDestLatLng;
+                routingControl.setWaypoints(routingControl.getWaypoints()); // Refresh route with updated waypoint
             });
         }
-    }
+    };
+    
 
     return (
         <div className="map-container">
@@ -168,7 +141,8 @@ const Map = () => {
                     setDestination={setDestination}
                     setSrc={setSrc}
                     setDest={setDest}
-                    handleRoute={handleRoute} />
+                    handleRoute={handleRoute}
+                />
             </div>
         </div>
     );
